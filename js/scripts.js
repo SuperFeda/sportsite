0936d4e6444f266f4b939bbc9a3c427a33308252
SuperFeda <112332753+SuperFeda@users.noreply.github.com>
2025-01-04 17:11:00 +0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -50,6 +50,8 @@ function BMR(age, height, weight, sex) {
         bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)
     } else if (sex === "man") {
         bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
+    } else if (sex === "hermaphrodite") {
+        bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
     }
     return bmr
 }
